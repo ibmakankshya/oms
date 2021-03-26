034fb4e1d342092dd5c09c23d38e9f759b9389e2
unknown <ibmjavanh06@iiht.tech>
2021-03-26 11:33:19 +0530

    added mongoDB

--- src/main/java/com/ibm/demo/Services/OrderService.java
+++ src/main/java/com/ibm/demo/Services/OrderService.java
@@ -3,17 +3,21 @@ package com.ibm.demo.Services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm.demo.entity.Order;
+import com.ibm.demo.repo.OrderRepository;
 
 @Service
 public class OrderService {
-
+	@Autowired     //dependency of repository class
+	OrderRepository orderepository;
+	
 	public String createOrder(Order order) {// spring beans
-
-		return "Order is made!";
-	}
+		Order savedOrder = orderepository.save(order) ;
+		return savedOrder.getID();
+		}
 	public List<Order> getOrders(){
 		return new ArrayList<Order>();
 	}
--- src/main/java/com/ibm/demo/entity/Order.java
+++ src/main/java/com/ibm/demo/entity/Order.java
@@ -3,11 +3,23 @@ package com.ibm.demo.entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.data.annotation.Id;
+
 public class Order {
 	@NotNull
 	@NotBlank //annotation for empty
 	private String item;
 	private int price;
+	@Id
+	private String ID;
+	
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
 
 	public String getItem() {
 		return item;
